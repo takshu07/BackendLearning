92a46eb143d8659e66c22c76ea91f3b906b5afe5
takshu07 <takshubudhlakoti123@gmail.com>
2025-03-11 15:03:08 +0530

    Backend befor express use

--- Server/index.js
+++ Server/index.js
@@ -6,12 +6,15 @@ const url=require("url");
 //creating a server using http module
 const myServer=httpModule.createServer((req,res)=>{
     // creating a log to store the time of the request recieved also tells where is the request headed
+    
     const log=`${Date.now()} ${req.url}: New request recieved\n`;
     // using url module we installed using nom i url , to parse our url and know better detail of url
+   
     const myurl=url.parse(req.url,true);
     console.log(myurl);
     
     fs.appendFile("log.txt",log,(err,data)=>{
+      
         //giving response based on where the request is sent for
         //multi route
         switch(myurl.pathname){
